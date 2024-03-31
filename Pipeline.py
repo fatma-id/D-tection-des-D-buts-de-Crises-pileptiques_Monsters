3d5a894dd9e48dab49e3d55f11b086e5f76b2ac4
fatma <besbesfatma25@gmail.com>
2024-03-31 08:59:53 +0200

    2nd

--- Pipeline.py
+++ Pipeline.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
@@ -13,80 +14,94 @@ import numpy as np
 import mlflow
 import mlflow.sklearn
 
-# DagsHub import
 import dagshub
+# Terminer le run actif s'il y en a un
+if mlflow.active_run():
+    mlflow.end_run()
 
-# Initialisez la session MLflow
-mlflow.set_tracking_uri("https://dagshub.com/fatma-id/MLOpsDetectionCrisesElleptiques.git")
-mlflow.set_experiment("experiment")
+# Définir le nom de votre nouvel experiment
+experiment_name = "experimentDetec"
 
-# Récupérer les données
-data = pd.read_csv('merged_data.csv')
+# Vérifier si l'expérience existe déjà
+experiment = mlflow.get_experiment_by_name(experiment_name)
+if experiment is None:
+    # Si l'expérience n'existe pas, créer une nouvelle expérience
+    mlflow.create_experiment(experiment_name)
 
-# Séparation des fonctionnalités et de la variable cible
-X = data.drop(columns=["label"])
-y = data["label"]
+# Récupérer l'ID de l'expérience
+experiment_id = mlflow.get_experiment_by_name(experiment_name).experiment_id
 
-# Sélectionner uniquement les colonnes numériques
-numeric_cols = X.select_dtypes(include=np.number).columns.tolist()
-X_numeric = X[numeric_cols]
+try:
+    # Démarrer un run
+    mlflow.start_run(experiment_id=experiment_id)
 
-# Séparation des données en ensembles d'entraînement et de test
-X_train, X_test, y_train, y_test = train_test_split(X_numeric , y, test_size=0.2, random_state=42)
+    # Récupérer les données
+    data = pd.read_csv('C:\\Users\\pc\\Desktop\\AI_Night_Challenge\\merged_data.csv')
 
-# Créer un pipeline avec la normalisation, la sélection de caractéristiques et le modèle
-def create_pipeline(model):
-    pipeline = Pipeline([
-        ('scaler', StandardScaler()),
-        ('selector', SelectKBest(score_func=f_classif)),
-        ('model', model)
-    ])
-    return pipeline
+    # Séparation des fonctionnalités et de la variable cible
+    X = data.drop(columns=["label"])
+    y = data["label"]
 
-# Définir les modèles à tester
-models = {
-    'SVM': SVC(),
-    'Random Forest': RandomForestClassifier(),
-    'Logistic Regression': LogisticRegression()
-}
+    # Sélectionner uniquement les colonnes numériques
+    numeric_cols = X.select_dtypes(include=np.number).columns.tolist()
+    X_numeric = X[numeric_cols]
 
-# Définir les paramètres pour la sélection de caractéristiques
-selector_params = {'selector__k': [5, 10, 15,20,23]}  # Nombre de caractéristiques à sélectionner
+    # Séparation des données en ensembles d'entraînement et de test
+    X_train, X_test, y_train, y_test = train_test_split(X_numeric , y, test_size=0.2, random_state=42)
 
-# Créer un objet de validation croisée
-kfold = KFold(n_splits=5, shuffle=True, random_state=42)
+    # Créer un pipeline avec la normalisation, la sélection de caractéristiques et le modèle
+    def create_pipeline(model):
+        pipeline = Pipeline([
+            ('scaler', StandardScaler()),
+            ('selector', SelectKBest(score_func=f_classif)),
+            ('model', model)
+        ])
+        return pipeline
 
-# Pour chaque modèle, évaluez sa performance avec la validation croisée
-for name, model in models.items():
-    pipeline = create_pipeline(model)
+    # Définir les modèles à tester
+    models = {
+        'SVM': SVC(),
+        'Random Forest': RandomForestClassifier(),
+        'Logistic Regression': LogisticRegression()
+    }
 
-    # Recherche sur grille avec validation croisée
-    with mlflow.start_run() as run:
-        grid_search = GridSearchCV(pipeline, param_grid=selector_params, cv=kfold, scoring='accuracy')
-        grid_search.fit(X_train, y_train)
+    # Définir les paramètres pour la sélection de caractéristiques
+    selector_params = {'selector__k': [5, 10, 15,20,23]}  # Nombre de caractéristiques à sélectionner
 
-        # Obtenir le meilleur score de validation croisée et les meilleurs paramètres
-        best_score = grid_search.best_score_
-        best_params = grid_search.best_params_
+    # Créer un objet de validation croisée
+    kfold = KFold(n_splits=5, shuffle=True, random_state=42)
 
-        # Évaluer le modèle sur l'ensemble de test
-        y_pred = grid_search.predict(X_test)
-        test_accuracy = accuracy_score(y_test, y_pred)
+    # Pour chaque modèle, évaluez sa performance avec la validation croisée
+    for name, model in models.items():
+        pipeline = create_pipeline(model)
 
-        # Enregistrer les métriques
-        mlflow.log_metric("best_score", best_score)
-        mlflow.log_metric("test_accuracy", test_accuracy)
+        # Recherche sur grille avec validation croisée
+        with mlflow.start_run(experiment_id=experiment_id) as run:
+            grid_search = GridSearchCV(pipeline, param_grid=selector_params, cv=kfold, scoring='accuracy')
+            grid_search.fit(X_train, y_train)
 
-        # Enregistrer les paramètres
-        mlflow.log_params(best_params)
+            # Obtenir le meilleur score de validation croisée et les meilleurs paramètres
+            best_score = grid_search.best_score_
+            best_params = grid_search.best_params_
 
-        # Enregistrer le modèle
-        mlflow.sklearn.log_model(grid_search.best_estimator_, "model")
+            # Évaluer le modèle sur l'ensemble de test
+            y_pred = grid_search.predict(X_test)
+            test_accuracy = accuracy_score(y_test, y_pred)
 
-        # Afficher les résultats
-        print(f"Modèle: {name}")
-        print(f"Meilleur score de validation croisée: {best_score}")
-        print(f"Meilleurs paramètres de sélection de caractéristiques: {best_params}")
-        print(f"Précision sur l'ensemble de test: {test_accuracy}")
-        print("---------------------------------------")
+            # Enregistrer les métriques
+            mlflow.log_metric("best_score", best_score)
+            mlflow.log_metric("test_accuracy", test_accuracy)
 
+            # Enregistrer les paramètres
+            mlflow.log_params(best_params)
+
+            # Enregistrer le modèle
+            mlflow.sklearn.log_model(grid_search.best_estimator_, "model")
+
+except Exception as e:
+    # Terminer le run actif en cas d'erreur
+    mlflow.end_run()
+    print("An error occurred:", e)
+finally:
+    # S'assurer que le run est terminé même en cas d'erreur
+    mlflow.end_run()
